438efa0eb52b26417242abcbe0db3c318caec5df
Aryanvish <aryanvishwakarma0622@gmail.com>
2025-08-24 13:32:10 +0530

    code 1

---
+++ Basics.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ; 
+
+int main(){
+    // code for printing Alphabets from A tob Z using while loop
+
+    int i = 1 ;
+    char n = 'A';
+
+    while(i<=26){
+        cout<<n<<endl;
+        n = 'A'+ i ; 
+        i++;
+    }
+
+
+
+}
