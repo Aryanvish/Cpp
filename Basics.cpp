b90dc45ef3cd9d2845939056ce583cfb8b4c3a6a
Aryanvish <aryanvishwakarma0622@gmail.com>
2025-08-27 08:18:06 +0530

    code 2

--- Basics.cpp
+++ Basics.cpp
@@ -2,16 +2,33 @@
 using namespace std ; 
 
 int main(){
-    // code for printing Alphabets from A tob Z using while loop
+//   code for telling whether the input n is a power of a number x asked by user Eg: the user asks n=100 to check whether its power of 2  
 
-    int i = 1 ;
-    char n = 'A';
+int i , n , x ; 
 
-    while(i<=26){
-        cout<<n<<endl;
-        n = 'A'+ i ; 
-        i++;
+//  Take the user input of number and the x in whose power u want to check
+
+cout<<"Enter the number whose power u want to check :";
+cin>>n;
+cout<<"Enter the number in whose power u want to check : ";
+cin>>x;
+
+if(n<=0){
+    cout<<"Enter a number greater than 0 as there is no such number which gives -ve and 0 as the ans when raised to a power";
+}
+if(n==1){
+    cout<<"1";
+}
+
+while(n!=1){
+    for(i=1;i<=x-1;i++){
+        if(n%x==i){
+            cout<<"0";
+        }
     }
+    n/=x;
+}
+cout<<"1" ;
 
 
 
