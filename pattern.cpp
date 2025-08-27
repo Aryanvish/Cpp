4b4a7be77172d9cee787ea78fcb74ae473925b70
Aryanvish <aryanvishwakarma0622@gmail.com>
2025-08-27 22:40:40 +0530

    pattern printing 4

--- pattern.cpp
+++ pattern.cpp
@@ -5,16 +5,16 @@ using namespace std ;
 int main(){
     int row , col ; 
 
-    //  print   * * * * *     for 5 rows
-            //  * * * *
-            //  * * *
-            //  * * 
-            //  * 
+    //  print 1 2 3 4 5    for 5 rows
+            //  
+            //   
+            //  
+            // 
     
 
     for(row=5;row>=1;row--){
-        for(col=row;col>=1;col--){
-            cout<<"* ";
+        for(col=1;col<=5;col++){
+            cout<<col<<" ";
         }cout<<endl;
 
     }
