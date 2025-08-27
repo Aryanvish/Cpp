c5a1ca1d85c5ff40ab935fe26643472c9e3163c4
Aryanvish <aryanvishwakarma0622@gmail.com>
2025-08-27 22:32:23 +0530

    pattern printing 3

--- pattern.cpp
+++ pattern.cpp
@@ -5,15 +5,15 @@ using namespace std ;
 int main(){
     int row , col ; 
 
-    //  print   *      for 5 rows
-            //  * *
-            //  * * *
+    //  print   * * * * *     for 5 rows
             //  * * * *
-            //  * * * * * 
+            //  * * *
+            //  * * 
+            //  * 
     
 
-    for(row=1;row<=5;row++){
-        for(col=1;col<=row;col++){
+    for(row=5;row>=1;row--){
+        for(col=row;col>=1;col--){
             cout<<"* ";
         }cout<<endl;
 
